07911403a921556ba6b95d0a59dffe9c3a807e10
Sergey <smikenin777@gmail.com>
2026-07-21 14:45:55 +0700

    Remove Next.js build files

--- .next/dev/server/app/api/results/route.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/results/route.js")
-R.c("server/chunks/app_api_results_route_ts_1c570_l._.js")
-R.c("server/chunks/node_modules_next_1851mha._.js")
-R.c("server/chunks/node_modules_@supabase_auth-js_dist_module_00x2u05._.js")
-R.c("server/chunks/node_modules_19slhkn._.js")
-R.c("server/chunks/[root-of-the-server]__0mixzox._.js")
-R.c("server/chunks/_next-internal_server_app_api_results_route_actions_1e7k4qb.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/results/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/results/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DOJ6l9RKNFRuEpxMAOIdDg3qQwJYdp6lf2p3/75FyzY=\"\n}"
